d186306e007383fdb101a4b9b27e969c2f6c8986
Bambang Adhitya <bambang@kiosawan.com>
2020-08-30 22:53:13 +0700

    Revisi

--- api/GetBanner.php
+++ api/GetBanner.php
@@ -1,6 +1,6 @@
 <?php
-if (checkIfKeyExist($PostData, [])) {
-	$data = $db->ExecuteAll("SELECT * FROM tb_banner ORDER BY isForBanner DESC", []);
+if (checkIfKeyExist($PostData, ["isMobile"])) {
+	$data = $db->ExecuteAll("SELECT * FROM tb_banner WHERE mobile=? ORDER BY isForBanner DESC", [$PostData->isMobile ? 1 : 0]);
 	$response->Success($data);
 } else {
 	$response->Error("Please Check Parameters");
--- api/UpdateBanner.php
+++ api/UpdateBanner.php
@@ -4,23 +4,31 @@ if (checkIfKeyExist($PostData, [])) {
 	if ($PostData->id) {
 		if ($PostData->delete) {
 			$db->Execute("DELETE FROM tb_banner WHERE id=?", [$PostData->id]);
-		$response->Success("Sukses hapus banner");
-	} else {
-		$db->Execute("UPDATE tb_banner SET isForBanner=?", ["0"]);
-		$db->Execute("UPDATE tb_banner SET isForBanner=? WHERE id=?", ["1", $PostData->id]);
-		$response->Success("Sukses ubah banner");
+			$response->Success("Sukses hapus banner");
+		} else {
+			if (checkIfKeyExist($PostData, ['isMobile'])) {
+				$db->Execute("UPDATE tb_banner SET isForBanner=? WHERE mobile=?", ["0", $PostData->isMobile ? 1 : 0]);
+				$db->Execute("UPDATE tb_banner SET isForBanner=? WHERE id=?", ["1", $PostData->id]);
+				$response->Success("Sukses ubah banner");
+			} else {
+				$response->Error("Please Check Parameters");
+			}
 		}
 	} else if ($PostData->data) {
-		$data = array_map(function ($a) use ($upload) {
-			$image = $upload->base64_to_file($a->file);
-			return $image;
-		}, $PostData->data);
-		$p = join(",", array_map(function () {
-			return "(?)";
-		}, $data));
-		$db->Execute("INSERT INTO tb_banner (image) VALUES $p", $data);
-		// $response->Success($p);
-		$response->Success("Sukses tambah banner");
+		if (checkIfKeyExist($PostData, ['isMobile'])) {
+			$data = array_map(function ($a) use ($upload) {
+				$image = $upload->base64_to_file($a->file);
+				return $image;
+			}, $PostData->data);
+			$isMobile = $PostData->isMobile ? 1 : 0;
+			$p = join(",", array_map(function () use ($isMobile) {
+				return "(?, '$isMobile')";
+			}, $data));
+			$db->Execute("INSERT INTO tb_banner (image, mobile) VALUES $p", $data);
+			$response->Success("Sukses upload banner");
+		} else {
+			$response->Error("Please Check Parameters");
+		}
 	} else {
 		$response->Error("No Action");
 	}
